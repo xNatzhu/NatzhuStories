c1a8c3b64ab7acbcbbc430f97911cdf68c0be658
xNatzhu <agustinnahuelsaravia@gmail.com>
2024-02-10 20:17:41 -0300

    creation proyect

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+import Home from "./pages/portal/home/Home.tsx"
+function App() {
+  return (
+    <>
+        <Home />
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/portal/home/Home.tsx
@@ -0,0 +1,10 @@
+function Home() {
+    return(
+        <div>
+            <h1 className="text-3xl font-bold underline text-[#Fff]">
+                Hello world!
+            </h1>
+        </div>
+    )
+}
+export default Home;
